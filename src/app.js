ce476b89dd243c4df94d14d0fb18d7dc14763a58
alexander-bh <alexanderbh.tonali@gmail.com>
2026-01-16 09:53:40 -0600

    implemetacion de flows

--- src/app.js
+++ src/app.js
@@ -7,8 +7,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/auth.routes"));
-app.use("/api/chatbots", require("./routes/chatbots.routes"));
-// app.use("/api/flows", require("./routes/flows.routes"));
+//app.use("/api/chatbots", require("./routes/chatbots.routes"));
+app.use("/api/flows", require("./routes/flows.routes"));
 // app.use("/api/flow-nodes", require("./routes/flow-nodes.routes"));
 
 
