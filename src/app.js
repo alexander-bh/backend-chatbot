9fce597391e52fee3f54fd9ad63bde58eec3d115
alexander-bh <alexanderbh.tonali@gmail.com>
2026-01-16 09:55:30 -0600

    eliminacion de chatbot y flows

--- src/app.js
+++ src/app.js
@@ -8,8 +8,8 @@ app.use(express.json());
 
 app.use("/api/auth", require("./routes/auth.routes"));
 //app.use("/api/chatbots", require("./routes/chatbots.routes"));
-app.use("/api/flows", require("./routes/flows.routes"));
-// app.use("/api/flow-nodes", require("./routes/flow-nodes.routes"));
+//app.use("/api/flows", require("./routes/flows.routes"));
+//app.use("/api/flow-nodes", require("./routes/flow-nodes.routes"));
 
 
 // Test routes
