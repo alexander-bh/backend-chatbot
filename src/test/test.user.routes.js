c1accebfa94d88976b807662e2bc20f5e46d9bfb
alexander-bh <alexanderbh.tonali@gmail.com>
2026-01-16 12:40:30 -0600

    test local

--- src/test/test.user.routes.js
+++ src/test/test.user.routes.js
@@ -4,19 +4,15 @@ const User = require("../models/User");
 
 router.get("/", async (req, res) => {
   try {
-   
     await connectDB();
-
-    
     const users = await User.find().lean();
-
     res.json({
       success: true,
       total: users.length,
       data: users
     });
   } catch (error) {
-    console.error("❌ ERROR REAL:", error);
+    console.error("ERROR REAL:", error);
 
     res.status(500).json({
       success: false,
