5a9e00dce8732fdc0972eee87c2a6af0bd7ee061
alexander-bh <alexanderbh.tonali@gmail.com>
2026-01-16 16:14:48 -0600

    weq

--- src/app.js
+++ src/app.js
@@ -13,9 +13,5 @@ app.get("/ping", (req, res) => {
 //prueba de rutas
 app.use("/test", require("./test/test-db.js"));
 app.use("/test-chatbot", require("./test/test.chatbot.routes.js"));
-app.use("/test-user", require("./test/test.user.routes.js"));
-
-//rutas principales
-app.use("/chatbots", require("./routes/chatbots.routes.js"));
 
 module.exports = app;
--- src/test/test.chatbot.routes.js
+++ src/test/test.chatbot.routes.js
@@ -1,20 +1,24 @@
 const router = require("express").Router();
+const connectDB = require("../config/database");
 const Chatbot = require("../models/Chatbot");
 
 router.get("/", async (req, res) => {
   try {
+    await connectDB(); 
     const chatbots = await Chatbot.find().lean();
 
-    res.json({
+    return res.json({
       success: true,
       total: chatbots.length,
       data: chatbots
     });
   } catch (error) {
-    console.error("ERROR CHATBOTS:", error);
-    res.status(500).json({ success: false, error: error.message });
+    console.error("ERROR CHATBOTS:", error.message);
+    return res.status(500).json({
+      success: false,
+      error: error.message
+    });
   }
 });
-
 
 module.exports = router;
