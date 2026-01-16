134eb61307acd76941c72c94fc728c8c89ce69b3
alexander-bh <alexanderbh.tonali@gmail.com>
2026-01-16 10:10:35 -0600

    modificcacion app_js

--- src/app.js
+++ src/app.js
@@ -6,14 +6,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", require("./routes/auth.routes"));
+//app.use("/api/auth", require("./routes/auth.routes"));
 //app.use("/api/chatbots", require("./routes/chatbots.routes"));
 //app.use("/api/flows", require("./routes/flows.routes"));
 //app.use("/api/flow-nodes", require("./routes/flow-nodes.routes"));
 
 
 // Test routes
-app.use("/api/test", require("./test/test.routes"));
+app.use("/test", require("./test/test.routes"));
 
 module.exports = app;
 
