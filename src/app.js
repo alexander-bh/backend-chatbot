db4f37c998e015f988291ed6740bd4d23dfe27f9
alexander-bh <alexanderbh.tonali@gmail.com>
2026-01-16 15:07:40 -0600

    point

--- src/app.js
+++ src/app.js
@@ -6,15 +6,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-//routes
-app.use("/auth", require("./routes/auth.routes"));
-app.use("/chatbots", require("./routes/chatbots.routes"));
-app.use("/users", require("./routes/users.routes"));
-app.use("/flows", require("./routes/flows.routes"));
-app.use("/flow-nodes", require("./routes/flow-nodes.routes"));
-
-// test route
-app.use("/test", require("./test/test.routes"));
-app.use("/test-users", require("./test/test.user.routes"));
+app.get("/ping", (req, res) => {
+  res.json({ ok: true });
+});
 
 module.exports = app;
