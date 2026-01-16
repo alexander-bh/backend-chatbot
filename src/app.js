84351c708d89c54840709a4e6a0efb2ba8e65a07
alexander-bh <alexanderbh.tonali@gmail.com>
2026-01-16 15:33:15 -0600

    sin mongo

--- src/app.js
+++ src/app.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-const connectDB = require("./config/database");
 
 const app = express();
-
-connectDB(); 
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/test", require("./test/test-db.js"));
-app.use("/test-users", require("./test/test.user.routes.js"));
-app.use("/test-chatbots", require("./test/test.chatbot.routes.js"));
+app.get("/ping", (req, res) => {
+  res.json({ ok: true });
+});
 
 module.exports = app;
