3209955c5b3945e6b212cab1e5c3097d109342fe
alexander-bh <alexanderbh.tonali@gmail.com>
2026-01-16 10:14:20 -0600

    test routers

--- src/test/test.routes.js
+++ src/test/test.routes.js
@@ -1,9 +1,17 @@
-const router = require("express").Router();
+/*const router = require("express").Router();
 const User = require("../models/User");
 
 router.get("/users", async (req, res) => {
   const users = await User.find();
   res.json(users);
+});
+
+module.exports = router;*/
+
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+  res.json({ ok: true });
 });
 
 module.exports = router;
