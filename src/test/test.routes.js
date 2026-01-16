44699ed65b9edf23e05faebdc10c590182f4ae86
alexander-bh <alexanderbh.tonali@gmail.com>
2026-01-16 10:34:18 -0600

    modificacion test-router

--- src/test/test.routes.js
+++ src/test/test.routes.js
@@ -1,7 +1,14 @@
 const router = require("express").Router();
+const connectDB = require("../config/db");
 
-router.get("/", (req, res) => {
-  res.json({ ok: true });
+router.get("/", async (req, res) => {
+  try {
+    await connectDB();
+    res.json({ ok: true });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
 });
 
 module.exports = router;
