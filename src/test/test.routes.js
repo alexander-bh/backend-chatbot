155cc247c69ae74a1eb1a471e406af57239874d1
alexander-bh <alexanderbh.tonali@gmail.com>
2026-01-16 10:36:00 -0600

    router test

--- src/test/test.routes.js
+++ src/test/test.routes.js
@@ -1,14 +1,7 @@
 const router = require("express").Router();
-const connectDB = require("../config/db");
 
-router.get("/", async (req, res) => {
-  try {
-    await connectDB();
-    res.json({ ok: true });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: error.message });
-  }
+router.get("/", (req, res) => {
+  res.json({ ok: true });
 });
 
 module.exports = router;
