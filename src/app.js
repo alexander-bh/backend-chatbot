7bc1615363f67751c31be2e212e19a7842f8c6e3
alexander-bh <alexanderbh.tonali@gmail.com>
2026-01-16 09:50:54 -0600

    Implementacion auth

--- src/app.js
+++ src/app.js
@@ -6,6 +6,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", require("./routes/auth.routes"));
+// app.use("/api/chatbots", require("./routes/chatbots.routes"));
+// app.use("/api/flows", require("./routes/flows.routes"));
+// app.use("/api/flow-nodes", require("./routes/flow-nodes.routes"));
+
+
+// Test routes
 app.use("/api/test", require("./test/test.routes"));
 
 module.exports = app;
