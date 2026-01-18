b2b42bb85009faf4227af62265957c8baedea304
alexander-bh <alexanderbh.tonali@gmail.com>
2026-01-17 20:47:12 -0600

    JWT

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,6 +1,10 @@
 const jwt = require("jsonwebtoken");
 
 exports.generateToken = (payload) => {
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET no definida");
+  }
+
   return jwt.sign(payload, process.env.JWT_SECRET, {
     expiresIn: "1d"
   });
