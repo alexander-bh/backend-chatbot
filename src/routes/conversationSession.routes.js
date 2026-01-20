fc21e9f3ec30b0e3485d91f90d464d71380872f6
alexander-bh <alexanderbh.tonali@gmail.com>
2026-01-20 11:24:33 -0600

    conversacion

--- src/routes/conversationSession.routes.js
+++ src/routes/conversationSession.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("../controllers/chatbotSettings.controller");
+const controller = require("../controllers/conversationSession.controller");
 const auth = require("../middlewares/auth.middleware");
 
 router.post("/start", auth, controller.startConversation);
