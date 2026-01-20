11a9807f4315b52ee5f922376b22473c6721c697
alexander-bh <alexanderbh.tonali@gmail.com>
2026-01-20 11:28:02 -0600

    Router

--- src/routes/conversationSession.routes.js
+++ src/routes/conversationSession.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("../controllers/conversationSession.controller");
+const controller = require("../controllers/conversationsession.controller");
 const auth = require("../middlewares/auth.middleware");
 
 router.post("/start", auth, controller.startConversation);
