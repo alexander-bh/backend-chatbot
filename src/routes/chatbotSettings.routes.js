af84a91bf37df6504c49cb41b92c7633c5c6523d
alexander-bh <alexanderbh.tonali@gmail.com>
2026-01-20 21:59:00 -0600

    bacteria

--- src/routes/chatbotSettings.routes.js
+++ src/routes/chatbotSettings.routes.js
@@ -4,26 +4,12 @@ const auth = require("../middlewares/auth.middleware");
 const upload = require("../middlewares/uploadAvatar.middleware");
 const settingsController = require("../controllers/chatbotSettings.controller");
 
-// Subir avatar
-router.post(
-  "/chatbots/:id/avatar",
-  auth,
-  upload.single("avatar"),
-  settingsController.uploadAvatar
-);
 
 // Obtener configuración de chatbot
 router.get(
   "/chatbots/:id/settings",
   auth,
   settingsController.getSettings
-);
-
-//Cuardar configuración de chatbot
-router.post(
-  "/chatbots/:id/settings",
-  auth,
-  settingsController.saveAllSettings
 );
 
 router.put(
