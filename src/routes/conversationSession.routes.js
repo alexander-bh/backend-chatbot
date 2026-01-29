1326c5d32257fd5e4ad70c60620d1a83ebe34b2f
alexander-bh <alexanderbh.tonali@gmail.com>
2026-01-29 12:33:39 -0600

    modificar

--- src/routes/conversationSession.routes.js
+++ src/routes/conversationSession.routes.js
@@ -4,6 +4,6 @@ const controller = require("../controllers/conversationsession.controller");
 const auth = require("../middlewares/auth.middleware");
 
 router.post("/start", auth, controller.startConversation);
-router.post("/next", auth, controller.nextStep);
+router.post("/:id/next", auth, controller.nextStep);
 
 module.exports = router;
