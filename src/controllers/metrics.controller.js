6ed4b838ca671d9095bd9d308fc7c6d00fc94f79
alexander-bh <alexanderbh.tonali@gmail.com>
2026-02-25 13:40:30 -0600

    Actulizar v4

--- src/controllers/metrics.controller.js
+++ src/controllers/metrics.controller.js
@@ -115,7 +115,8 @@ exports.getNodeFunnel = async (req, res) => {
       {
         $match: {
           chatbot_id: chatbotObjectId,
-          account_id: new mongoose.Types.ObjectId(accountId)
+          account_id: new mongoose.Types.ObjectId(accountId),
+          flow_id: flow._id
         }
       },
 
