7eed4e4a25552c31342a1086af8a42a4b8e17587
alexander-bh <alexanderbh.tonali@gmail.com>
2026-03-06 01:41:13 -0600

    Actualizar

--- src/routes/media.routes.js
+++ src/routes/media.routes.js
@@ -1,8 +1,8 @@
+const router = require("express").Router();
 const uploadMedia = require("../middlewares/uploadMedia");
 const chatbotController = require("../controllers/multimedia.controller")
 const auth = require("../middlewares/auth.middleware");
 const role = require("../middlewares/role.middleware");
-
 
 router.use(auth);
 router.use(role("ADMIN","CLIENT"));
