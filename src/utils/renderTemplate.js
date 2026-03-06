65df72bc877a40f093d1db2779187a3df7c1d71a
alexander-bh <alexanderbh.tonali@gmail.com>
2026-03-06 17:11:17 -0600

    Actualizacion de los nodos

--- src/utils/renderTemplate.js
+++ src/utils/renderTemplate.js
@@ -1,4 +1,4 @@
-// utils/renderTemplate Este archivo ya no se usa 
+// utils/renderTemplate Esta se usa 
 module.exports = function renderTemplate(template, variables = {}) {
   return template.replace(/{{\s*(\w+)\s*}}/g, (_, key) => {
     return variables[key] ?? "";
