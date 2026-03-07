3edb64ede47b806c0b7bbcbb917e021b1e908613
alexander-bh <alexanderbh.tonali@gmail.com>
2026-03-06 18:29:06 -0600

    Actualizar los nodos

--- src/routes/flows.routes.js
+++ src/routes/flows.routes.js
@@ -3,20 +3,20 @@ const router = express.Router();
 const auth = require("../middlewares/auth.middleware");
 const role = require("../middlewares/role.middleware");
 const flowController = require("../controllers/flows.controller");
-const FLOW_ROLE = role("ADMIN", "CLIENT");
+const uploadMedia = require("../middlewares/uploadMedia");
+router.use(auth);
+router.use(role("ADMIN", "CLIENT"));
 
 // Guardar cambios
 router.post(
   "/:id/save",
-  auth,
-  FLOW_ROLE,
   flowController.saveFlow
 );
 
 // Obtener nodos por flow
 router.get(
   "/:flowId",
-  auth,
+  uploadMedia.any(),
   flowController.getNodesByFlow
 );
 
