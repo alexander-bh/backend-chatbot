7902adeb8da76054ff26ac4b472321dbb2e7d114
alexander-bh <alexanderbh.tonali@gmail.com>
2026-03-09 15:10:22 -0600

    Atualizar el video

--- src/integration/chatbotIntegration.controller.js
+++ src/integration/chatbotIntegration.controller.js
@@ -237,8 +237,9 @@ exports.renderEmbed = async (req, res) => {
         `script-src 'self' ${apiOrigin}`,
         `style-src 'self' 'unsafe-inline'`,
         `img-src 'self' data: https:`,
-        `media-src https:`,
+        `media-src https:`,          // ← AGREGA ESTA LÍNEA
         `connect-src 'self' ${apiOrigin} wss:`,
+        `frame-ancestors ${frameAncestors}`
       ].join("; ")
     );
 
