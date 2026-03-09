8b2d11c68fabc68ff13936b5b89fe15c45aad036
alexander-bh <alexanderbh.tonali@gmail.com>
2026-03-08 20:30:30 -0600

    Actualizar el pat

--- src/controllers/media.controller.js
+++ src/controllers/media.controller.js
@@ -43,44 +43,6 @@ exports.deleteMedia = async (req, res) => {
   }
 };
 
-exports.replaceMedia = async (req, res) => {
-  try {
-    const { old_public_id } = req.body;
-
-    const newFile = req.file;
-
-    if (!newFile) {
-      return res.status(400).json({
-        success: false,
-        message: "Archivo requerido"
-      });
-    }
-
-    // borrar anterior
-    if (old_public_id) {
-      try {
-        await deleteFromCloudinary(old_public_id);
-      } catch (err) {
-        console.log("Error borrando anterior:", err.message);
-      }
-    }
-
-    return res.json({
-      success: true,
-      url: newFile.path,
-      public_id: newFile.filename,
-      type: newFile.mimetype.startsWith("video/") ? "video" : "image"
-    });
-
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      success: false,
-      message: "Error reemplazando media"
-    });
-  }
-};
-
 exports.deleteNodeMedia = async (req, res) => {
   try {
 
@@ -144,3 +106,42 @@ exports.deleteNodeMedia = async (req, res) => {
     });
   }
 };
+
+exports.replaceMedia = async (req, res) => {
+  try {
+    const { old_public_id } = req.body;
+
+    const newFile = req.file;
+
+    if (!newFile) {
+      return res.status(400).json({
+        success: false,
+        message: "Archivo requerido"
+      });
+    }
+
+    // borrar anterior
+    if (old_public_id) {
+      try {
+        await deleteFromCloudinary(old_public_id);
+      } catch (err) {
+        console.log("Error borrando anterior:", err.message);
+      }
+    }
+
+    return res.json({
+      success: true,
+      url: newFile.path,
+      public_id: newFile.filename,
+      type: newFile.mimetype.startsWith("video/") ? "video" : "image"
+    });
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Error reemplazando media"
+    });
+  }
+};
+
