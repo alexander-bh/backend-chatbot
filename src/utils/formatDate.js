328d5fe8345851d0053e7821da20732c07ee9d2e
alexander-bh <alexanderbh.tonali@gmail.com>
2026-03-12 19:28:39 -0600

    Actualizar la fecha

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,9 +1,18 @@
 // utils/formatDate.js
+
 const dayjs = require("dayjs");
+const utc = require("dayjs/plugin/utc");
+const timezone = require("dayjs/plugin/timezone");
 require("dayjs/locale/es");
 
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
 const formatDateAMPM = (date) => {
+  if (!date) return null;
+
   return dayjs(date)
+    .tz("America/Mexico_City")
     .locale("es")
     .format("DD/MM/YYYY, hh:mm A")
     .replace("AM", "a.m")
