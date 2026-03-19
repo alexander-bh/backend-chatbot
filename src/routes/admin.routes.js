91c2fd05257adf0a5c70c210f496a7803d1ba98b
alexander-bh <alexanderbh.tonali@gmail.com>
2026-03-19 09:49:19 -0600

    neo morfo

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -4,91 +4,62 @@ const auth = require("../middlewares/auth.middleware");
 const role = require("../middlewares/role.middleware");
 const adminController = require("../controllers/admin.controller");
 const upload = require("../middlewares/uploadAvatar.middleware");
-
 router.use(auth);
-
 /* =========================
    GLOBAL FLOW
 ========================= */
-
 // Puede verlo cualquier usuario autenticado
 router.get("/flow", adminController.getFlowDetail);
-
-
 /* =========================
    DASHBOARD
 ========================= */
-
 router.get("/dashboard", role("ADMIN"), adminController.getDashboard);
-
-
 /* =========================
    USERS (ADMIN)
 ========================= */
-
 router.get("/users", role("ADMIN"), adminController.getAllUsers);
 router.get("/users/:id", role("ADMIN"), adminController.getUserDetail);
 router.put("/users/:id", role("ADMIN"), adminController.updateAnyUser);
 router.delete("/users/:id", role("ADMIN"), adminController.deleteAnyUser);
-
-
 /* =========================
    ACCOUNTS (ADMIN)
 ========================= */
-
 router.get("/accounts", role("ADMIN"), adminController.getAllAccounts);
-
-
 /* =========================
    CHATBOTS
 ========================= */
-
 // Crear chatbot → solo ADMIN
 router.post("/chatbots", role("ADMIN"), adminController.createChatbotForUser);
-
 // Listar chatbots → solo ADMIN
 router.get("/chatbots", role("ADMIN"), adminController.getAllChatbots);
-
 // Detalle → solo ADMIN
 router.get("/chatbots/:id", role("ADMIN"), adminController.getChatbotDetail);
-
 // Eliminar → solo ADMIN
 router.delete("/chatbots/:id", role("ADMIN"), adminController.deleteAnyChatbot);
-
 // Editar → solo ADMIN
 router.put("/chatbots/:id", role("ADMIN"), upload.single("avatar"),adminController.updateAnyChatbot);
-
-
 /* =========================
    AVATARS ADMIN
 ========================= */
-
 router.get("/chatbots/:id/avatars", role("ADMIN"), adminController.getAvailableAvatars);
 router.delete("/chatbots/:id/deleteAvatar", role("ADMIN"), adminController.deleteAvatar);
 router.patch("/chatbots/:id/toggle", role("ADMIN"), adminController.toggleChatbot);
 router.post("/:publicId/token/regenerate", role("ADMIN"), adminController.regenerateInstallToken);
-
 /* =========================
    AUDITORÍAS
 ========================= */
-
 router.get("/audit", role("ADMIN"), adminController.getAuditLogs);
 router.post("/register", role("ADMIN"), adminController.createUserByAdmin);
-
 /* =========================
    AVATARES GLOBALES
 ========================= */
-
 router.post("/avatars", role("ADMIN"), upload.single("avatar"), adminController.createAvatar);
 router.get("/avatars", role("ADMIN"), adminController.getAllAvatars);
 router.delete("/avatars/:id", role("ADMIN"), adminController.deleteAvatarGlobal);
 router.patch("/avatars/:id/set-default", role("ADMIN"), adminController.setDefaultAvatar);
-
-
 /* =========================
    CONTACTO GLOBAL
 ========================= */
-
 router.post("/templates", role("ADMIN"), adminController.createDefaultContactTemplate);
 router.get("/templates", role("ADMIN"), adminController.getDefaultContactTemplates);
 router.put("/templates/:id", role("ADMIN"), adminController.updateDefaultContactTemplate);
@@ -96,8 +67,6 @@ router.delete("/templates/:id", role("ADMIN"), adminController.deleteDefaultCont
 router.get("/templates/deleted", role("ADMIN"), adminController.getDeletedDefaultContactTemplates);
 router.patch("/templates/:id/restore", role("ADMIN"), adminController.restoreDefaultContactTemplate);
 router.delete("/templates/:id/permanent", role("ADMIN"), adminController.permanentlyDeleteDefaultContactTemplate);
-
-
 /* =========================
    SYSTEM CONFIG
 ========================= */
