9837c9e1583eef5b99197c606bd0d105dd22805a
alexander-bh <alexanderbh.tonali@gmail.com>
2026-03-18 22:56:18 -0600

    Actualizacion del index

--- api/index.js
+++ api/index.js
@@ -1,3 +1,9 @@
 const app = require("../src/app");
 
-module.exports = app;
+module.exports = (req, res) => {
+  if (req.path.startsWith("/api/cron")) {
+    return res.status(404).end();
+  }
+
+  return app(req, res);
+};
