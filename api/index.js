cce73d915ff8e30b62396749329beae80d9a7c9e
alexander-bh <alexanderbh.tonali@gmail.com>
2026-03-18 23:02:34 -0600

    Actualizar los docuemnto

--- api/index.js
+++ api/index.js
@@ -1,9 +1,3 @@
 const app = require("../src/app");
 
-module.exports = (req, res) => {
-  if (req.path.startsWith("/api/cron")) {
-    return res.status(404).end();
-  }
-
-  return app(req, res);
-};
+module.exports = app;
